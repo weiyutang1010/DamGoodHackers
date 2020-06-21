f2f3ca2b77da6a90169ef8b9dfa1bf5015d29663
weiyutang1010 <weiyutang1010@outlook.com>
2020-06-20 23:42:24 -0700

    Updated the styling for results card

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,13 +5,26 @@ import React from 'react'
 export default function About() {
     return (
         <div style={aboutStyle}>
-            <h1>Cheer Me Up!</h1>
-            <p>{cheermeup}</p>
-            <h1>About Us</h1>
-            {person1.printing()}
-            {person2.printing()}
-            {person3.printing()}
-            {person4.printing()}
+            <div>
+                <h1>Cheer Me Up!</h1>
+                <p style={{
+                    whiteSpace:'pre-wrap',
+                    paddingLeft:'3%',
+                    marginTop:'1%',
+                }}>{cheermeup}</p>
+            </div>
+            <div style={{marginTop: "2%"}}>
+                <h1>About Us</h1>
+                <div style={{
+                    paddingLeft:'3%',
+                    marginTop:'1%',
+                }}>
+                    {person1.printing()}
+                    {person2.printing()}
+                    {person3.printing()}
+                    {person4.printing()}
+                </div>
+            </div>
         </div>
     )
 }
@@ -26,27 +39,33 @@ class Person {
         return (
             <div className="Person">
                 <h2>{this.name}</h2>
-                <p>{this.description}</p>
+                <p style={{
+                    whiteSpace: 'pre-wrap',
+                    marginBottom: '1.5%',
+                }}>{this.description}</p>
             </div>
         )
     }
 }
 
-const desc1 = `Hello World, 12345678910, abcdefg`;
-const desc2 = `Hello World`;
-const desc3 = `Hello World`;
-const desc4 = `Hello World`;
+const desc1 = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic veniam voluptatem expedita sed sequi fuga eos molestiae praesentium. Minima autem aliquid quae quibusdam, nulla asperiores voluptatem mollitia distinctio dolores doloribus totam nisi nihil! Voluptatum, tenetur veritatis illo cumque magni dolorem corrupti, labore saepe commodi quibusdam magnam nostrum, repellat ea officiis?`;
+const desc2 = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic veniam voluptatem expedita sed sequi fuga eos molestiae praesentium. Minima autem aliquid quae quibusdam, nulla asperiores voluptatem mollitia distinctio dolores doloribus totam nisi nihil! Voluptatum, tenetur veritatis illo cumque magni dolorem corrupti, labore saepe commodi quibusdam magnam nostrum, repellat ea officiis?`;
+const desc3 = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic veniam voluptatem expedita sed sequi fuga eos molestiae praesentium. Minima autem aliquid quae quibusdam, nulla asperiores voluptatem mollitia distinctio dolores doloribus totam nisi nihil! Voluptatum, tenetur veritatis illo cumque magni dolorem corrupti, labore saepe commodi quibusdam magnam nostrum, repellat ea officiis?`;
+const desc4 = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic veniam voluptatem expedita sed sequi fuga eos molestiae praesentium. Minima autem aliquid quae quibusdam, nulla asperiores voluptatem mollitia distinctio dolores doloribus totam nisi nihil! Voluptatum, tenetur veritatis illo cumque magni dolorem corrupti, labore saepe commodi quibusdam magnam nostrum, repellat ea officiis?`;
 
 const person1 = new Person("Wei Yu", desc1);
 const person2 = new Person("Person2", desc2);
 const person3 = new Person("Person3", desc3);
 const person4 = new Person("Person4", desc4);
 
-const cheermeup = `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis cumque laborum ex numquam expedita nostrum pariatur suscipit obcaecati commodi. Fugit nemo, porro in quia laborum facilis rerum ratione! Optio veritatis blanditiis autem ipsum voluptatum dicta quibusdam harum quo repudiandae possimus.`
+const cheermeup = `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis cumque laborum ex numquam expedita nostrum pariatur suscipit obcaecati commodi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate nostrum suscipit voluptate. Itaque repellendus magnam cumque quaerat non? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium illum vero earum nostrum commodi voluptatibus labore corrupti libero debitis obcaecati. Distinctio ex aliquid quod, accusamus commodi deserunt quaerat fuga odit sequi! Autem?
+
+Fugit nemo, porro in quia laborum facilis rerum ratione! Optio veritatis blanditiis autem ipsum voluptatum dicta quibusdam harum quo repudiandae possimus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta accusantium modi, ratione iure vero inventore laborum!`
 
 
 const aboutStyle = {
     position: 'absolute',
-    top: '40px',
-    padding: '30px',
+    width: '80%',
+    top: '13%',
+    left: '10%'
 }
--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -26,7 +26,7 @@ export default function Homepage() {
 // Placeholder for the story, Might consider connecting it with database
 const storyTitle1 = `Story Title`;
 // Issue with separating paragraph
-const storyContent1 = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque hendrerit felis purus, nec interdum felis finibus nec. Pellentesque sodales dignissim ipsum a hendrerit. Donec at nisi sed odio mollis bibendum. Sed commodo sapien quis massa varius, vitae ullamcorper nunc suscipit. Etiam elementum ipsum vitae hendrerit hendrerit. Duis cursus suscipit mattis. Etiam fringilla, arcu vel interdum pellentesque, nisl ligula scelerisque enim, eget bibendum mauris nunc at est. Phasellus urna lectus, luctus vel tortor ut, semper consectetur sapien. Aenean semper pharetra massa, et cursus sem gravida in. Donec lobortis enim eget pellentesque elementum. Nullam at nisi odio. Vestibulum sed mi leo. In elementum imperdiet ultricies. Aliquam laoreet dapibus risus, eget finibus purus egestas sed. Cras id mauris rhoncus, ornare metus id, sollicitudin tellus.
+const storyContent1 = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque hendrerit felis purus, nec interdum felis finibus nec. Pellentesque sodales dignissim ipsum a hendrerit. Donec at nisi sed odio mollis bibendum. Sed commodo sapien quis massa varius, vitae ullamcorper nunc suscipit. Etiam elementum ipsum vitae hendrerit hendrerit. Duis cursus suscipit mattis. Etiam fringilla, arcu vel interdum pellentesque, nisl ligula scelerisque enim, eget bibendum mauris nunc at est. Phasellus urna lectus, luctus vel tortor ut, semper consectetur sapien. Aenean semper pharetra massa, et cursus sem gravida in. Donec lobortis enim eget pellentesque elementum. Nullam at nisi odio. Vestibulum sed mi leo. In elementum imperdiet ultricies. 
 
 Nam rutrum maximus sapien, quis scelerisque felis imperdiet et. Donec dictum est sit amet dapibus euismod. Nam in augue ac eros volutpat finibus. Aliquam ut porttitor urna, eget molestie velit. Etiam cursus faucibus turpis. Pellentesque laoreet quam porttitor urna commodo, eget sollicitudin arcu sagittis. Sed auctor erat nec neque accumsan viverra. Fusce eu vestibulum sem.`;
 const storyImage1 = image1;
@@ -46,8 +46,8 @@ const storyStyle = {
     // float: 'right',
     display: 'grid',
     gridTemplateColumns: '37% auto',
-    gridTemplateRows: '7% auto',
+    gridTemplateRows: '6% auto',
     //background: '#333',
     width: "100%",
-    marginTop: '30px',
+    marginTop: '20px',
 }
