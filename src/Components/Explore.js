7448365dcbc40316ae972052e98f0fc22ef6a19b
weiyutang1010 <weiyutang1010@outlook.com>
2020-06-21 00:25:07 -0700

    Set up styling for all the pages

--- src/Components/Explore.js
+++ src/Components/Explore.js
@@ -5,33 +5,37 @@ import image3 from './Images/Samoyed.png'
 import image4 from './Images/CoolCat.jpg'
 import Story from './Story/Story'
 
+const desc1 = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita sunt minima repudiandae. In atque reprehenderit, dolore aliquam magnam laudantium. Porro explicabo harum asperiores eveniet est deleniti nisi laborum? 
+
+Optio omnis quidem obcaecati cum corrupti minus accusamus ad vel corporis, voluptatem et dicta nisi dolorum inventore eum, sequi excepturi voluptatum! Eligendi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem mollitia dolorem id.`
+
 export default class Explore extends Component {
     state = [{
                 title: "Title1",
-                desc: "Description1",
+                desc: desc1,
                 image: image1,
-                width: "190px",
+                width: "210px",
                 alt: "Alternative text1"
             },
             {
                 title: "Title2",
-                desc: "Description2",
+                desc: desc1,
                 image: image2,
-                width: "190px",
+                width: "210px",
                 alt: "Alternative text2"
             },
             {
                 title: "Title3",
-                desc: "Description3",
+                desc: desc1,
                 image: image3,
-                width: "190px",
+                width: "210px",
                 alt: "Alternative text3"
             },
             {
                 title: "Title4",
-                desc: "Description4",
+                desc: desc1,
                 image: image4,
-                width: "190px",
+                width: "210px",
                 alt: "Alternative text4"
             },]
     
@@ -47,9 +51,9 @@ export default class Explore extends Component {
                 style={storyStyle}
                 />
                 <hr style={{
-                    position: 'absolute',
-                    left: '-150%',
-                    width: '100vw',
+                    transform: 'translate(-20%, 0)',
+                    width: '150vw',
+                    color: '#ccc',
                 }}></hr>
             </div>
         )
@@ -58,7 +62,11 @@ export default class Explore extends Component {
     render() {
         console.log(this.state);
         return (
-            <div className="stories" style={pageStyle}>
+            <div id="stories" style={pageStyle}>
+                <h2 style={{
+                    fontSize: '35px',
+                    marginBottom: '10px',
+                }}>Explore</h2>
                 {/* Still thinking about how to do these with a loop */}
                 {this.createStory(0)}
                 {this.createStory(1)}
@@ -72,9 +80,19 @@ export default class Explore extends Component {
 const pageStyle = {
     position: 'absolute',
     top: '80px',
-    left: '20%'
+    left: '15%',
+    width: '70%',
+    height: '200%',
 }
 
 const storyStyle = {
-    backgroundColor: '#FFF4D2'
+    display: 'grid',
+    gridTemplateColumns: '25% auto',
+    gridTemplateRows: '20% auto',
+    backgroundColor: '#FFF4D2',
+    margin: '20px 0',
+    height: '40vh',
+    boxShadow: '5px 5px 3px 4px #ccc',
+    overflow: 'hidden',
+    padding: '20px',
 }
